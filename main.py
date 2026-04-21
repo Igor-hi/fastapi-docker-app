8e9dd852f1484ebaaf13d5175103c066ed5a7c82
Igor-hi <voronovigor772@gmail.com>
2026-04-21 08:35:37 +0700

    feat: initial FastAPI app with GET  and POST endpoints

---
+++ main.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hi i  am a FastAapi server"}
+
+@app.get("/hello/{name}")
+def hello_name(name:str ):
+    return {"message": f"Hi, {name}"}
+
+class Item(BaseModel):
+    name: str
+    price: float
+    in_stock: bool = True
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {"received": item, "message": "Add"}
